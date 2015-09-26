ea21ad4d18a11c7f02c66e285dfadde09d2a1bf3
Chris Loew <cloew123@gmail.com>
2015-09-26 13:18:15 -0600

    Added basic toffl logic

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='toggl_driver',
+      version='0.0.1',
+      description="",
+      author='',
+      author_email='',
+      packages=['toggl_driver'],
+     )
