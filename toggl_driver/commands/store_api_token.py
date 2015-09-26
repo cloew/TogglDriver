1aae08391031ce8f4d460c12da2cd7562ee900b9
Chris Loew <cloew123@gmail.com>
2015-09-26 13:29:41 -0600

    Added basic Command hierarchy

---
+++ toggl_driver/commands/store_api_token.py
@@ -0,0 +1,16 @@
+
+class StoreApiToken:
+    """ Represents a command to store the global Api Token for connecting to Toggl """
+    description = "Store the Toggl API Token"
+    
+    def addArguments(self, parser):
+        """ Add arguments to the parser """
+        parser.add_argument('token', action='store', help='Token to store')
+        
+    def run(self, arguments):
+        """ Run the command """
+        self.store(arguments.token)
+        
+    def store(self, token):
+        """ Store the token """
+        pass
