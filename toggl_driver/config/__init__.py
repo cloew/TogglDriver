0ed137118b1f162d866911ec79a0156b31df1ed6
Chris Loew <cloew123@gmail.com>
2015-09-28 14:28:06 -0600

    Added local config

--- toggl_driver/config/__init__.py
+++ toggl_driver/config/__init__.py
@@ -1,1 +1,2 @@
 from .global_config import GlobalConfig
+from .local_config import LocalConfig
---
+++ toggl_driver/config/local_config.py
@@ -0,0 +1,29 @@
+from .global_config import GlobalConfig
+
+from kao_config import LocalConfigFile
+from kao_decorators import lazy_property
+
+class LocalConfig:
+    """ Represents the local Toggl Driver Configuration """
+    CONFIG_FILENAME = ".toggl-settings"
+    
+    @lazy_property
+    def config(self):
+        """ Return the local config file """
+        return LocalConfigFile(self.CONFIG_FILENAME, create=True)
+    
+    @lazy_property
+    def pid(self):
+        """ Return the project id """
+        return self.config.readlines()[0].strip()
+    
+    @lazy_property
+    def project(self):
+        """ Return the stored project """
+        return GlobalConfig.connection.projects.withId(self.pid)
+            
+    def storeProjectId(self, id):
+        """ Store the provided id """
+        return self.config.write(id)
+            
+LocalConfig = LocalConfig()
