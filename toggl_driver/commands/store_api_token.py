f37d825198ca870fa51acc188b49a4fa22eb0470
Chris Loew <cloew123@gmail.com>
2015-09-27 19:35:48 -0600

    Updated commands to the latest KaoCommand

--- toggl_driver/commands/store_api_token.py
+++ toggl_driver/commands/store_api_token.py
@@ -1,16 +1,14 @@
 from ..config import GlobalConfig
+from kao_command.args import Arg
 
 class StoreApiToken:
     """ Represents a command to store the global Api Token for connecting to Toggl """
     description = "Store the Toggl API Token"
-    
-    def addArguments(self, parser):
-        """ Add arguments to the parser """
-        parser.add_argument('token', action='store', help='Token to store')
+    args = [Arg('token', action='store', help='Token to store')]
         
-    def run(self, arguments):
+    def run(self, *, token):
         """ Run the command """
-        self.store(arguments.token)
+        self.store(token)
         
     def store(self, token):
         """ Store the token """
