54550f94113be9136693bed6d065a4c5e9eabb25
Chris Loew <cloew123@gmail.com>
2015-09-27 19:37:07 -0600

    Switched to an actual file for configuration

--- toggl_driver/config/global_config.py
+++ toggl_driver/config/global_config.py
@@ -1,30 +1,24 @@
-from kao_config import GlobalConfigDir
+from kao_config import GlobalConfigFile
 from kao_decorators import lazy_property
 
 class GlobalConfig:
     """ Represents the global Toggl Driver Configuration """
-    DIR_NAME = ".toggl-driver"
-    TOKEN_FILENAME = ".token"
+    CONFIG_FILENAME = ".toggl-driver"
     
     @lazy_property
-    def configDir(self):
-        """ Return the global config directory """
-        return GlobalConfigDir(self.DIR_NAME, create=True)
-    
-    @lazy_property
-    def tokenFilename(self):
-        """ Return the global config filename """
-        return self.configDir.getFilename(self.TOKEN_FILENAME, create=True)
+    def config(self):
+        """ Return the global config file """
+        return GlobalConfigFile(self.CONFIG_FILENAME, create=True)
     
     @lazy_property
     def token(self):
         """ Return the global config Api Token """
-        with open(self.tokenFilename, 'r') as file:
+        with open(self.config.path, 'r') as file:
             return file.readlines()[0].strip()
             
     def storeToken(self, token):
         """ Store the provided token """
-        with open(self.tokenFilename, 'w') as file:
+        with open(self.config.path, 'w') as file:
             return file.write(token)
             
 GlobalConfig = GlobalConfig()
